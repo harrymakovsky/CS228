ea45e3fbd5f0b9496abdd2483366f1221c401455
harrymakovsky <hmakovsk@uvm.edu>
2020-09-06 16:32:29 -0400

    Finished step 59

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,14 +5,20 @@ var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
 Leap.loop(controllerOptions, function(frame)
 {
-    clear();
-    x += Math.floor(Math.random()*3)-1;
-    y += Math.floor(Math.random()*3)-1;
+    //clear();
+    //x += Math.floor(Math.random()*3)-1;
+    //y += Math.floor(Math.random()*3)-1;
 
-    circle(x,y,50);
+    //circle(x,y,50);
 
-    console.log(i);
-    i+=1;
+    //console.log(i);
+    //i+=1;
+    if (frame.hands.length==1){
+        console.log(frame.hands[0]);
+        var fingers = frame.hands[0].fingers;
+        console.log(fingers);
+
+    } 
 
 }
 );
