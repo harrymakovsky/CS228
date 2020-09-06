f1a2e8622d82948ea4bef33d98d03f0b608f26c4
harrymakovsky <hmakovsk@uvm.edu>
2020-09-06 16:19:14 -0400

    Step 50 done

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,9 +1,15 @@
 var controllerOptions = {};
 
 var i = 0;
-
+var x = window.innerWidth / 2; 
+var y = window.innerHeight / 2;
 Leap.loop(controllerOptions, function(frame)
 {
+    clear();
+    x += Math.floor(Math.random()*3)-1;
+
+    circle(x,y,50);
+
     console.log(i);
     i+=1;
 
