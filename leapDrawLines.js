8308cddc785f65559b8694e42134b5127d24ae43
harrymakovsky <hmakovsk@uvm.edu>
2020-09-13 14:02:26 -0400

    Drawing circles for each bone

---
+++ leapDrawLines.js
@@ -0,0 +1,108 @@
+var controllerOptions = {};
+
+var i = 0;
+var x = window.innerWidth / 2; 
+var y = window.innerHeight / 2;
+var z = 50;
+var rawXMin = 10000;
+var rawXMax = -10000;
+var rawYMin = 10000;
+var rawYMax = -10000;
+
+
+Leap.loop(controllerOptions, function(frame)
+{
+    clear();
+    HandleFrame(frame);
+}
+);
+
+function HandleFrame(frame){
+
+    //x += Math.floor(Math.random()*3)-1;
+    //y += Math.floor(Math.random()*3)-1;
+
+    
+    if (frame.hands.length==1){
+        var hand = frame.hands[0];
+        HandleHand(hand);
+    } 
+
+}
+
+function HandleHand(hand){
+        var fingers = hand.fingers;
+
+        for(var i = 0; i<5 ; i++){
+            HandleFinger(fingers[i]);
+            
+        }
+
+}
+
+function HandleFinger(finger){
+
+
+    for(var i = 0; i<4 ; i++){
+        HandleBone(finger.bones[i]);
+    }
+
+//    if(finger.tipPosition[0]< rawXMin){
+//        rawXMin = finger.tipPosition[0];
+//    }
+//    if(finger.tipPosition[0]> rawXMax){
+//        rawXMin = finger.tipPosition[0];
+//    }
+//    
+//    if(finger.tipPosition[1]< rawYMin){
+//        rawYMin = finger.tipPosition[1];
+//    }
+//    if(finger.tipPosition[1]> rawYMax){
+//        rawYMax = finger.tipPosition[1];
+//    }
+// 
+//    x = scaleValue(finger.tipPosition[0],[rawXMin,rawXMax],[0,window.innerWidth]);
+//    y = window.innerHeight - scaleValue(finger.tipPosition[1],[rawYMin,rawYMax],[0,window.innerHeight]);
+//    z = finger.tipPosition[2] + 400;
+//
+//    
+//    circle(x,y,50);
+}
+
+
+function HandleBone(bone){
+    var base = bone.prevJoint;
+    console.log(bone);
+
+    if(base[0]< rawXMin){
+        rawXMin = base[0];
+    }
+    if(base[0]> rawXMax){
+        rawXMax = base[0];
+    }
+    if(base[1]< rawYMin){
+        rawYMin = base[1];
+    }
+    if(base[1]> rawYMax){
+        rawYMax = base[1];
+    }
+ 
+    x = scaleValue(base[0],[rawXMin,rawXMax],[0,window.innerWidth]);
+    y = window.innerHeight - scaleValue(base[1],[rawYMin,rawYMax],[0,window.innerHeight]);
+    z = base[2] + 400;
+
+    
+    circle(x,y,50);
+
+
+
+}
+
+//Found this code on github
+function scaleValue(value, from, to) {
+
+	var scale = (to[1] - to[0]) / (from[1] - from[0]);
+	var capped = Math.min(from[1], Math.max(from[0], value)) - from[0];
+
+	return ~~(capped * scale + to[0]);
+}
